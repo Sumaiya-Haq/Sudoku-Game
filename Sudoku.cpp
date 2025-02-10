4e2973ad1513ecc3b6af497bb5f972331b77b8d5
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-02-10 19:35:47 +0600

    check number validation

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -21,10 +21,42 @@ void Board_Size(int board[9][9],int size)
             }
          }
 }
-bool number_valid(int board[9][9]){
+int number_valid(int board[9][9]){
     int row,col;
     int num,size;
-    
+    for (int m = 0; m <size; m++)
+    {
+       if(board[row][x]==num||board[x][col]==num){
+        return 0;
+       }
+    }
+    int first_row=row-row%3;
+    int first_col=col-col%3;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        {
+            if(board[i+first_row][j+first_col]==num){
+                return 0;
+            }
+        }
+    }
+   return 1; 
+}
+
+int find_empty_cell(int board[9][9],int &row,int &col,int size)
+{
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++)
+        {
+            if(board[i][j]==0)
+            {
+                row=i;
+                col=j;
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 int main()
 {
