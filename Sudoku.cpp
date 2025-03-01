771c394691a4960ea63406acac541f06c0e8e8ee
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-03-01 22:39:23 +0600

    solved

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,64 +1,118 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void Board_Size(int board[9][9],int size)
+void printBoard(int board[9][9], int size) 
 {
-    int i,j;
-    for(i=0;i<size;i++)
-    {
-        for(j=0;j<size;j++)
+    for (int i = 0; i < size; i++)
+     {
+        for (int j = 0; j < size; j++) 
         {
-            cout<<board[i][j]<<" ";
-            if((j+1)%3==0&&j!=size-1)
-            {
-                cout<<"| ";
+            cout << board[i][j] << " ";
+            if ((j + 1) % 3 == 0 && j != size - 1)
+             {
+                cout << "| ";
             }
         }
-            cout<<endl;
-            if((i+1)%3==0&&i!=size-1)
-            {
-                cout<<"-------------------"<<endl;
-            }
-         }
-}
-int number_valid(int board[9][9]){
-    int row,col;
-    int num,size;
-    for (int m = 0; m <size; m++)
-    {
-       if(board[row][x]==num||board[x][col]==num){
-        return 0;
-       }
+        cout << endl;
+        if ((i + 1) % 3 == 0 && i != size - 1)
+         {
+            cout << "-------------------" << endl;
+        }
     }
-    int first_row=row-row%3;
-    int first_col=col-col%3;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)
+}
+
+
+int number_valid(int board[9][9], int row, int col, int num, int size) 
+{
+    for (int x = 0; x < size; x++)
+     {
+        if (board[row][x] == num || board[x][col] == num) 
         {
-            if(board[i+first_row][j+first_col]==num){
+            return 0;
+        }
+    }
+
+    int first_row = row - row % 3;
+    int first_col = col - col % 3;
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
+            if (board[i + first_row][j + first_col] == num)
+             {
                 return 0;
             }
         }
     }
-   return 1; 
+    return 1;
 }
 
-int find_empty_cell(int board[9][9],int &row,int &col,int size)
-{
-    for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++)
-        {
-            if(board[i][j]==0)
-            {
-                row=i;
-                col=j;
+int find_empty_cell(int board[9][9], int &row, int &col, int size)
+ {
+    for (int i = 0; i < size; i++)
+     {
+        for (int j = 0; j < size; j++)
+         {
+            if (board[i][j] == 0) 
+            { 
+                row = i;
+                col = j;
                 return 1;
             }
         }
     }
     return 0;
 }
-int main()
-{
 
+
+int solveSudoku(int board[9][9], int size) 
+{
+    int row, col;
+    if (find_empty_cell(board, row, col, size) == 0)
+    {
+        return 1; 
+    }
+
+    for (int num = 1; num <= size; num++) {
+        if (number_valid(board, row, col, num, size) == 1)
+         {
+            board[row][col] = num;
+
+            if (solveSudoku(board, size) == 1)
+             {
+                return 1; 
+            }
+
+            board[row][col] = 0; 
+        }
+    }
+    return 0; 
+}
+
+int main() {
+    const int size = 9;
+    int board[size][size];
+
+    cout << "Enter the Sudoku puzzle (use 0 for empty spaces):" << endl;
+    for (int i = 0; i < size; i++)
+     {
+        for (int j = 0; j < size; j++)
+         {
+            cin >> board[i][j];
+        }
+    }
+
+    cout << "Original Sudoku Puzzle:" << endl;
+    printBoard(board, size);
+
+    if (solveSudoku(board, size) == 1)
+     {
+        cout << "\nSolved Sudoku Puzzle:" << endl;
+        printBoard(board, size);
+    } 
+    else {
+        cout << "\nNo solution exists for the given Sudoku puzzle.\n";
+    }
+
+    return 0;
 }
